de61a84d65c732af4882461edb41f9c510b3e3b0
Day Vazquez <dayvazquez@protonmail.com>
2022-08-25 21:10:05 +0200

    Updated calendar

--- calendar/calendar.c
+++ calendar/calendar.c
@@ -1,24 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-char* get_month_name(int month, char *name);
-char* get_day_name(int day, char *name);
-int get_days_in_month(int month, int year);
-int get_leap_year(int year);
-void print_calendar(int year);
-int get_day_of_week(int month, int year);
-
-int main(int argc, char *argv[]) {
-    int day, month, year;
-    char mName[20];
-
-    printf("Enter the year: ");
-    scanf("%d", &year);
-    printf("\n");
-    printf("%d\n\n", year);
-    print_calendar(year);
-    return 0;
-}
+#define LEAP_YEAR(Y) (Y % 4 == 0 && (Y % 100 != 0 || Y % 400 == 0))
 
 char* get_month_name(int month, char *mName) {
     switch (month) {
@@ -65,33 +48,6 @@ char* get_month_name(int month, char *mName) {
     return mName;
 }
 
-char* get_day_name(int day, char *dName) {
-    switch (day) {
-        case 1:
-            strcpy(dName, "Sunday");
-            break;
-        case 2:
-            strcpy(dName, "Monday");
-            break;
-        case 3:
-            strcpy(dName, "Tuesday");
-            break;
-        case 4:
-            strcpy(dName, "Wednesday");
-            break;
-        case 5:
-            strcpy(dName, "Thursday");
-            break;
-        case 6:
-            strcpy(dName, "Friday");
-            break;
-        case 7:
-            strcpy(dName, "Saturday");
-            break;
-    }
-    return dName;
-}
-
 int get_days_in_month(int month, int year) {
     int days;
     switch (month) {
@@ -111,47 +67,54 @@ int get_days_in_month(int month, int year) {
             days = 30;
             break;
         case 2:
-            days = get_leap_year(year);
+            days = LEAP_YEAR(year) ? 29 : 28;
             break;
     }
     return days;
 }
 
-int get_leap_year(int year) {
-    int leap;
-    if (year % 4 == 0 && year % 100 != 0) {
-        leap = 29;
-    } else {
-        leap = 28;
-    }
-    return leap;
-}
-
-void print_calendar(int year) {
-    int month, days;
-    char mName[20], dName[20];
-
-    for (month = 1; month <= 12; month++) {
-        days = get_days_in_month(month, year);
-        get_month_name(month, mName);
-        printf("%s\n", mName);
-        printf(" M  T  W  T  F  S  S\n");
-        
-        for (int i = 1; i <= days; i++) {
-            printf("%2d ", i);
-            if (i % 7 == 0 || i == days) {
-                printf("\n");
-            }
-        }
-        printf("\n");
-    }
-    
-}
-
-// Return index of day of week
+// Returns the day of the week of the first day of the given month and year
 int get_day_of_week(int month, int year) {
     int day, days;
     days = get_days_in_month(month, year);
     day = (year + year / 4 - year / 100 + year / 400 + (13 * month + 8) / 5 + days) % 7;
     return day;
+}
+
+// Function that prints months names and days of the week
+void print_month(int month, int year) {
+    int i, j, k, days, day;
+    char mName[10];
+    days = get_days_in_month(month, year);
+    day = get_day_of_week(month, year);
+    printf("\n%s %d\n", get_month_name(month, mName), year);
+    printf(" S  M  T  W  T  F  S\n");
+    for (i = 0; i < day; i++) {
+        printf("   ");
+    }
+    for (j = 1; j <= days; j++) {
+        printf("%2d ", j);
+        if ((j + day) % 7 == 0) {
+            printf("\n");
+        }
+    }
+    printf("\n");
+}
+
+void print_calendar(int year) {
+    int month, day, days, start;
+    char mName[20];
+
+    start = get_day_of_week(1, year);
+    for (month = 1; month <= 12; month++) {
+        print_month(month, year);
+    }
+}
+
+int main() {
+    int year;
+    printf("Enter year: ");
+    scanf("%d", &year);
+    print_calendar(year);
+    return 0;
 }
